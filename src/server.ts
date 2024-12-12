0ef18745485aa3fa17b6c5208a401fe0745a3ec9
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-11 22:30:57 -0300

    Creating another endpoint, and organizing teams info and pilots info into a vetors of objects, to organize the minimal API

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,26 @@ import fastify from "fastify";
 
 const server = fastify({logger: true});
 
+const teams = [
+    {id: 1, name: "McLaren", base: "woking, United Kingdom"},
+    {id: 2, name: "Mercedes", base: "Brackley, United Kingdom"},
+    {id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom"},
+]
+
+const drivers = [
+    {id: 1, name: "Max Verstappen", team: "Red Bull Racing"},
+    {id: 2, name: "Airton Senna", team: "McLaren"},
+    {id: 3, name: "Lewis Hamilton", team: "Mercedes"}
+]
+
 server.get("/teams", async (request, response) => {
     response.type("application/json").code(200)
-    return [{id: 1, name: "ferrari"}];
+    return {teams}
+})
+
+server.get("/drivers", async(request, response)=>{
+    response.type("applicaiton/json").code(200);
+    return {drivers}
 })
 
 server.listen({port: 3333}, ()=>{
